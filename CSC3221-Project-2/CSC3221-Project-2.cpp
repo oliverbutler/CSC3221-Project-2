e6f9f174582dbbd56b91cc4932328ca3cb6e1904
Oliver Butler <dev@oliverbutler.uk>
2020-12-28 16:53:29 +0000

    Add project files.

---
+++ CSC3221-Project-2/CSC3221-Project-2.cpp
@@ -0,0 +1,8 @@
+// CSC3221-Project-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!\n";
+}
